853760f13e32c6c3c6669b22e22de115a65abfdb
Pixules <119787718+Pixules@users.noreply.github.com>
2023-01-30 19:01:10 +0000

    some interpolation / simplifying in JS

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 function showSelectedCountry(event) {
-  if (event.target.value === "paris") {
-    alert(
-      `It is ${moment()
-        .tz("Europe/Paris")
-        .format("dddd, MMMM Do, YYYY h:mm a")} in Paris, Europe`
-    );
-  }
-  if (event.target.value === "tokyo") {
-    alert(
-      `It is ${moment()
-        .tz("Asia/Tokyo")
-        .format("dddd, MMMM Do, YYYY h:mm a")} in Tokyo, Japan`
-    );
-  }
-  if (event.target.value === "sydney") {
-    alert(
-      `It is ${moment()
-        .tz("Australia/Sydney")
-        .format("dddd, MMMM Do, YYYY h:mm a")} in Sydney, Australia`
-    );
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM Do, YYYY h:mm a");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
