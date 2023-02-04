2848ca2376a2c257570f521eb4fdf385df08aaa4
Pixules <119787718+Pixules@users.noreply.github.com>
2023-02-04 17:04:59 +0000

    updated so feeding back accurate time for locations
    small adjustment in html, removing unneeded code and adding JS to update time accurate for timezone

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function updateTime() {
+  let firstCityElement = document.querySelector("#first-location");
+  let firstCityDate = firstCityElement.querySelector(".date");
+  let firstCityTimeElement = firstCityElement.querySelector(".time");
+  let firstCityTime = moment().tz("America/Los_Angeles");
+
+  firstCityDate.innerHTML = firstCityTime.format("MMMM Do YYYY");
+  firstCityTimeElement.innerHTML = firstCityTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let secondCityElement = document.querySelector("#second-location");
+  let secondCityDate = secondCityElement.querySelector(".date");
+  let secondCityTimeElement = secondCityElement.querySelector(".time");
+  let secondCityTime = moment().tz("Australia/Adelaide");
+
+  secondCityDate.innerHTML = secondCityTime.format("MMMM Do YYYY");
+  secondCityTimeElement.innerHTML = secondCityTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
 function showSelectedCountry(event) {
   if (event.target.value.length > 0) {
     let currentTime = moment()
