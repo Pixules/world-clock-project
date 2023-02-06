8d63a00bb3096193d244c0cc4846bc11348f91c2
Pixules <119787718+Pixules@users.noreply.github.com>
2023-02-06 14:11:58 +0000

    added drop down functionality
    added with append to current display, will work on updating time and refining

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 function updateTime() {
   let firstCityElement = document.querySelector("#first-location");
-  let firstCityDate = firstCityElement.querySelector(".date");
-  let firstCityTimeElement = firstCityElement.querySelector(".time");
-  let firstCityTime = moment().tz("America/Los_Angeles");
+  if (firstCityElement) {
+    let firstCityDate = firstCityElement.querySelector(".date");
+    let firstCityTimeElement = firstCityElement.querySelector(".time");
+    let firstCityTime = moment().tz("America/Los_Angeles");
 
-  firstCityDate.innerHTML = firstCityTime.format("MMMM Do YYYY");
-  firstCityTimeElement.innerHTML = firstCityTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    firstCityDate.innerHTML = firstCityTime.format("MMMM Do YYYY");
+    firstCityTimeElement.innerHTML = firstCityTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let secondCityElement = document.querySelector("#second-location");
-  let secondCityDate = secondCityElement.querySelector(".date");
-  let secondCityTimeElement = secondCityElement.querySelector(".time");
-  let secondCityTime = moment().tz("Australia/Adelaide");
+  if (secondCityElement) {
+    let secondCityDate = secondCityElement.querySelector(".date");
+    let secondCityTimeElement = secondCityElement.querySelector(".time");
+    let secondCityTime = moment().tz("Australia/Adelaide");
 
-  secondCityDate.innerHTML = secondCityTime.format("MMMM Do YYYY");
-  secondCityTimeElement.innerHTML = secondCityTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    secondCityDate.innerHTML = secondCityTime.format("MMMM Do YYYY");
+    secondCityTimeElement.innerHTML = secondCityTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
@@ -25,11 +29,19 @@ setInterval(updateTime, 1000);
 
 function showSelectedCountry(event) {
   if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM Do, YYYY h:mm a");
-
-    alert(`It is ${currentTime} in ${event.target.value}`);
+    let currentTimeZone = event.target.value;
+    let cityName = currentTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(event.target.value);
+    let cityElement = document.querySelector("#locations");
+    cityElement.innerHTML += `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
   }
 }
 
